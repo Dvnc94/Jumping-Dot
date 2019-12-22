3ad819af7dd43f62dd8956e9131f5fac4136d8f7
Divyanshi Srivastava <divyanshi1350@gmail.com>
2019-12-21 16:18:44 -0800

    adding unity code

---
+++ Assets/Scripts/Dot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dot : MonoBehaviour {
+    public float upForce = 300f;
+    public AudioSource tapAudio;
+    public AudioSource dieAudio;
+    private bool isDead = false;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    void Update()
+    {
+        if(isDead == false) {
+            if(Input.GetMouseButtonDown(0)) {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+                tapAudio.Play();
+            }
+        }
+    }
+
+    void OnCollisionEnter2D() {
+        rb2d.simulated = false;
+        isDead = true;
+        GameControl.instance.PlayerDied();
+        dieAudio.Play();
+    }
+}
---
+++ Assets/Scripts/PolePool.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PolePool : MonoBehaviour
+{
+    public int polePoolSize = 5;
+    public GameObject polePrefab;
+    public float spawnRate = 4;
+    public float poleMin = -1f;
+    public float poleMax = 3.5f;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private GameObject[] poles;
+    private float timeSinceLastSpawned;
+    private float spawnXPosition = 5;
+    private int currentPole = 0;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        poles = new GameObject[polePoolSize];
+        for (int i = 0; i < polePoolSize; i++)
+        {
+            poles[i] = (GameObject) Instantiate (polePrefab, objectPoolPosition, Quaternion.identity);
+        }   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+        if(GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate) {
+            timeSinceLastSpawned = 0;
+            float spawnYPosition = Random.Range(poleMin, poleMax);
+            poles[currentPole].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentPole++;
+            if(currentPole >= polePoolSize) {
+                currentPole = 0;
+            }
+        }
+    }
+}
